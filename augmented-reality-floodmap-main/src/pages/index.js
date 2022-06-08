0bb7667b0f3d5ec99213bcc0aa8df7e5eac2ac10
Jen <91272778+jennefercampoverde@users.noreply.github.com>
2022-06-08 16:08:03 -0400

    Add files via upload

---
+++ augmented-reality-floodmap-main/src/pages/index.js
@@ -0,0 +1,16 @@
+//home page
+import * as React from 'react'
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>WELCOME!</h1>
+      <p>The NYC DOT's Augmented Reality Floodmap gives both desktop and mobile users a way to 
+        experience the effects of nuisance flooding in their area.
+      </p>
+    </main>
+  )
+}
+
+export default IndexPage
